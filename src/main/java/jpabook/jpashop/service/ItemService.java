f0f6680db80dfc7a0621c97377e6d364a9948899
toneofrain <toneofrain@yonsei.ac.kr>
2022-09-05 22:11:19 +0900

    feat: 최종

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,13 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+//    @Transactional
+//    public void updateItem(Long itemId, Book param) {
+//        Item findItem = itemRepository.findOne(itemId);
+//        findItem.setPrice(param.getPrice());
+//        findItem.setName();
+//    }
+
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
@@ -27,5 +35,6 @@ public class ItemService {
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
+
 
 }
